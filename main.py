2104062c38385e61bda1c3dc4886659538bf76d6
Srujith Poondla <spoondla@wisc.edu>
2018-04-30 21:53:26 -0500

    Modified logging

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from train import train
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-parser.add_argument('--batch-size', type=int, default=1000, metavar='N',
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
-parser.add_argument('--test-batch-size', type=int, default=34, metavar='N',
+parser.add_argument('--test-batch-size', type=int, default=64, metavar='N',
                     help='input batch size for testing (default: 1000)')
 parser.add_argument('--epochs', type=int, default=20, metavar='N',
                     help='number of epochs to train (default: 10)')
@@ -21,7 +21,7 @@ parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                     help='SGD momentum (default: 0.5)')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+parser.add_argument('--log-interval', type=int, default=50, metavar='N',
                     help='how many batches to wait before logging training status')
 parser.add_argument('--num-processes', type=int, default=2, metavar='N',
                     help='how many training processes to use (default: 2)')
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import os
 import timeit
-
+import time
 import pymp
 import torch
 import torch.optim as optim
@@ -9,7 +9,6 @@ from torch.autograd import Variable
 from torchvision import datasets, transforms
 from torch import multiprocessing as mp
 import redis
-import sgd
 from redis import StrictRedis
 from rediscluster import StrictRedisCluster
 
@@ -19,9 +18,9 @@ from common_functions import push_params_redis, get_shapes, get_params_redis, se
 
 def train(args, model):
     # startup_nodes = [{"host": "127.0.0.1", "port": "30001"},{"host": "127.0.0.1", "port": "30002"}]
+    # db = StrictRedisCluster(startup_nodes=startup_nodes, decode_responses=True)
     db = redis.ConnectionPool(host='localhost', port=6379, db=0)
     db = redis.StrictRedis(connection_pool=db)
-    # db = StrictRedisCluster(startup_nodes=startup_nodes, decode_responses=True)
 
     params_exists = check_param_exists(model, db)
     if not params_exists:
@@ -49,23 +48,26 @@ def train(args, model):
             batch_size=args.batch_size, shuffle=True, num_workers=1)
         # Optimizer declaration
         optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+        if not p.thread_num:
+            time.sleep(5)
 
         for epoch in range(args.epochs):
             epoch_start_time = timeit.default_timer()
-            loss = train_process(p.thread_num, optimizer, train_loader, model, args, shapes, db, epoch)
+            train_process(p.thread_num, optimizer, train_loader, model, args, shapes, db, epoch, epoch_start_time)
             epoch_time = epoch_time + timeit.default_timer() - epoch_start_time
             # if p.thread_num :
-            print('PID{}\tTrain Epoch: {}\t time: {} \tLoss: {:.6f}'.format(p.thread_num, epoch, epoch_time, loss))
-            # test_epoch(model, test_loader)
+            # print('PID{}\tTrain Epoch: {}\t time: {} \tLoss: {:.6f}'.format(p.thread_num, epoch, epoch_time, loss))
+            test_epoch(model, test_loader)
+            # print('PID{}\tTrain Epoch: {}\t time: {} \tLoss: {:.6f}'.format(p.thread_num, epoch, epoch_time, test_loss))
 
 
 def shuffle_tensor(tensor):
     return torch.randperm(tensor.size(0)).long()
 
 
-def train_process(thread_num, optimizer, train_loader, model, args, shapes, db, epoch):
+def train_process(thread_num, optimizer, train_loader, model, args, shapes, db, epoch, epoch_start_time):
+    # epoch_start_time = timeit.default_timer()
     for batch_idx, (data, target) in enumerate(train_loader):
-
         set_params(optimizer, get_params_redis(db, shapes))
         model.train()
         optimizer.zero_grad()
@@ -79,7 +81,12 @@ def train_process(thread_num, optimizer, train_loader, model, args, shapes, db, 
 
         loss_, params = optimizer.step()
         push_params_redis(params, db)
-    return loss.data[0]
+        if batch_idx % args.log_interval == 0:
+            print('{}\tTrain Epoch: {} [{}/{} ({:.0f}%)]\ttime: {}\tLoss: {:.6f}'.format(
+                thread_num, epoch, batch_idx * len(data), len(train_loader.dataset),
+                            100. * batch_idx / len(train_loader), timeit.default_timer()-epoch_start_time, loss.data[0]))
+
+    # return loss.data[0]
 
 
 def train_common(epoch, args, model, data_loader, optimizer):
@@ -110,4 +117,6 @@ def test_epoch(model, data_loader):
         correct += pred.eq(target.data).cpu().sum()
 
     test_loss /= len(data_loader.dataset)
-    return test_loss
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(data_loader.dataset),
+        100. * correct / len(data_loader.dataset)))

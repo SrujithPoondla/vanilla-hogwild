a46ca1e6ff4304fa96452e650ef6cc1c43d8074e
Srujith Poondla <spoondla@wisc.edu>
2018-05-09 12:35:06 -0500

    Added splitting datasets between nodes working V0.1

--- Datasets/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from .cifar import CIFAR10, CIFAR100
-from .mnist import MNIST, FashionMNIST
-
-__all__ = ('LSUN', 'LSUNClass',
-           'ImageFolder', 'FakeData',
-           'CocoCaptions', 'CocoDetection',
-           'CIFAR10', 'CIFAR100', 'FashionMNIST',
-           'MNIST', 'STL10', 'SVHN', 'PhotoTour', 'SEMEION')
